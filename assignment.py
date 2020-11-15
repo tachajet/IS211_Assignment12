c474a7c6d501074679d2eb7499aea8501a6aadae
Tzvi A Chajet <tac2197@082-tac2197-ml1.adcu.columbia.edu>
2020-11-14 21:07:30 -0500

    Stuck on dashboard.html not diaplying data correctly.

--- assignment.py
+++ assignment.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, g
 from os import urandom
 import sqlite3 as sq
+
+        
 app = Flask(__name__)
 sec_key=repr(urandom(24))
 app.secret_key=sec_key
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sq.connect('hw12.db')
+    return db
 @app.route('/', methods=['GET', 'POST'])
 def start():
     return render_template('login.html')
@@ -20,13 +27,18 @@ def login():
         return render_template('/login.html', methods=['GET', 'POST'], wrong_creds=wrong_creds)
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    data_link=sq.connect('hw12.db')
-    d=data_link.cursor()
-    d.execute('SELECT * FROM Students')
-    student_rows=d.fetchall()
+    global student_rows
     if 'username' not in session:
         return redirect('/login')
     else:
+        g.d=g.db.cursor()
+        g.d.execute('SELECT * FROM Students')
+        student_rows=g.d.fetchall()
         return render_template("dashboard.html", methods=['GET', 'POST'], student_rows=student_rows)
+@app.teardown_request
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 if __name__=="__main__":
     app.run(debug=True)
